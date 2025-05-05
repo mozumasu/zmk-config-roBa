3be760309506ff47d0007e4b4e943ec345a08422
mozumasu <ouri1229@gmail.com>
2025-05-05 22:38:55 +0900

    fix: 🐛 refactor keymap definitions for Japanese input language settings

--- config/keymap_jp.h
+++ config/keymap_jp.h
@@ -1,10 +1,10 @@
-#define JP_ZKHK  GRAVE        // Zenkaku ↔ Hankaku ↔ Kanji (半角 ↔ 全角 ↔ 漢字)
+#define JP_EISU  LANGUAGE_2   // 英数
+#define JP_KANA  LANG1        // かな
 #define JP_MINUS MINUS        // -
 #define JP_CARET EQUAL        // ^
 #define JP_YEN   INT3         // ¥
 #define JP_AT    LBKT         // @
 #define JP_LBKT  RBKT         // [
-#define JP_EISU  CAPS         // Eisū (英数)
 #define JP_SEMI  SEMI         // ;
 #define JP_COLON SQT          // :
 #define JP_RBKT  NUHS         // ]
@@ -14,7 +14,6 @@
 #define JP_BSLH  INT1         // (backslash)
 #define JP_MHEN  INT5         // Muhenkan (無変換)
 #define JP_HENK  INT4         // Henkan (変換)
-#define JP_KANA  INT2         // Katakana ↔ Hiragana ↔ Rōmaji (カタカナ ↔ ひらがな ↔ ローマ字)
 #define JP_EXCL  LS(N1)       // !
 #define JP_DQT   LS(N2)       // "
 #define JP_HASH  LS(N3)       // #
@@ -29,7 +28,6 @@
 #define JP_PIPE  LS(JP_YEN)   // |
 #define JP_GRAVE LS(JP_AT)    // `
 #define JP_LBRC  LS(JP_LBKT)  // {
-#define JP_CAPS  LS(JP_EISU)  // Caps Lock
 #define JP_PLUS  LS(JP_SEMI)  // +
 #define JP_ASTRK LS(JP_COLON) // *
 #define JP_RBRC  LS(JP_RBKT)  // }
