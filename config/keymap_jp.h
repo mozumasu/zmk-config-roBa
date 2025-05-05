af21dc554fa1073992cc80f0faf42ddbf4e0b63c
mozumasu <ouri1229@gmail.com>
2025-05-05 11:45:16 +0900

    feat: ✨ 標準コードの再定義だとビルドが通らないため戻す

--- config/keymap_jp.h
+++ config/keymap_jp.h
@@ -1,86 +1,39 @@
-// JIS配列に対応するZMK標準キーコード定義
-// Keymap Editor上で直接使える形式
-
-// 直接入力キー
-#undef ZKHK
-#define ZKHK   GRAVE       // 半角/全角
-/*#undef MINUS
-#define MINUS  MINUS       // -*/
-#undef CARET
-#define CARET  EQUAL       // ^
-#undef YEN
-#define YEN    INT3        // ¥
-#undef AT
-#define AT     LBKT        // @
-#undef LBKT
-#define LBKT   RBKT        // [
-#undef SEMI
-#define SEMI   SEMI        // ;
-#undef COLON
-#define COLON  SQT         // :
-#undef RBKT
-#define RBKT   BSLH        // ]
-#undef COMMA
-#define COMMA  COMMA       // ,
-#undef DOT
-#define DOT    DOT         // .
-#undef SLASH
-#define SLASH  SLASH       // /
-#undef BSLH
-#define BSLH   INT1        // \ (ろキー)
-
-// IME切替関連
-#undef MHEN
-#define MHEN   INT5        // 無変換
-#undef HENK
-#define HENK   INT4        // 変換
-#undef KANA
-#define KANA   LANG1       // かな
-#undef EISU
-#define EISU   LANG2       // 英数
-
-// 記号（Shift併用）
-#undef EXCL
-#define EXCL   LS(N1)      // !
-#undef DQT
-#define DQT    LS(N2)      // "
-#undef HASH
-#define HASH   LS(N3)      // #
-#undef DLLR
-#define DLLR   LS(N4)      // $
-#undef PRCNT
-#define PRCNT  LS(N5)      // %
-#undef AMPS
-#define AMPS   LS(N6)      // &
-#undef SQT
-#define SQT    LS(N7)      // '
-#undef LPAR
-#define LPAR   LS(N8)      // (
-#undef RPAR
-#define RPAR   LS(N9)      // )
-#undef EQUAL
-#define EQUAL  LS(MINUS)   // =
-#undef TILDE
-#define TILDE  LS(CARET)   // ~
-#undef PIPE
-#define PIPE   PIPE2       // |
-#undef GRAVE
-#define GRAVE  LS(AT)      // `
-#undef LBRC
-#define LBRC   LS(LBKT)    // {
-#undef CAPS
-#define CAPS   LS(CAPS)    // Caps Lock
-#undef PLUS
-#define PLUS   LS(SEMI)    // +
-#undef ASTRK
-#define ASTRK  LS(COLON)   // *
-#undef RBRC
-#define RBRC   LS(RBKT)    // }
-#undef LT
-#define LT     LS(COMMA)   // <
-#undef RT
-#define RT     LS(DOT)     // >
-#undef QMARK
-#define QMARK  LS(SLASH)   // ?
-#undef UNDER
-#define UNDER  LS(BSLH)    // _
+#define JP_ZKHK  GRAVE        // Zenkaku ↔ Hankaku ↔ Kanji (半角 ↔ 全角 ↔ 漢字)
+#define JP_MINUS MINUS        // -
+#define JP_CARET EQUAL        // ^
+#define JP_YEN   INT3         // ¥
+#define JP_AT    LBKT         // @
+#define JP_LBKT  RBKT         // [
+#define JP_EISU  CAPS         // Eisū (英数)
+#define JP_SEMI  SEMI         // ;
+#define JP_COLON SQT          // :
+#define JP_RBKT  NUHS         // ]
+#define JP_COMMA COMMA        // ,
+#define JP_DOT   DOT          // .
+#define JP_SLASH SLASH        // /
+#define JP_BSLH  INT1         // (backslash)
+#define JP_MHEN  INT5         // Muhenkan (無変換)
+#define JP_HENK  INT4         // Henkan (変換)
+#define JP_KANA  INT2         // Katakana ↔ Hiragana ↔ Rōmaji (カタカナ ↔ ひらがな ↔ ローマ字)
+#define JP_EXCL  LS(N1)       // !
+#define JP_DQT   LS(N2)       // "
+#define JP_HASH  LS(N3)       // #
+#define JP_DLLR  LS(N4)       // $
+#define JP_PRCNT LS(N5)       // %
+#define JP_AMPS  LS(N6)       // &
+#define JP_SQT   LS(N7)       // '
+#define JP_LPAR  LS(N8)       // (
+#define JP_RPAR  LS(N9)       // )
+#define JP_EQUAL LS(JP_MINUS) // =
+#define JP_TILDE LS(JP_CARET) // ~
+#define JP_PIPE  LS(JP_YEN)   // |
+#define JP_GRAVE LS(JP_AT)    // `
+#define JP_LBRC  LS(JP_LBKT)  // {
+#define JP_CAPS  LS(JP_EISU)  // Caps Lock
+#define JP_PLUS  LS(JP_SEMI)  // +
+#define JP_ASTRK LS(JP_COLON) // *
+#define JP_RBRC  LS(JP_RBKT)  // }
+#define JP_LT    LS(JP_COMMA) // <
+#define JP_RT    LS(JP_DOT)   // >
+#define JP_QMARK LS(JP_SLASH) // ?
+#define JP_UNDER LS(JP_BSLH)  // _
